35382cab691c8f6ff9d5dc71b1b468114a13dcde
NowaysZ <106604407+jeoifnwenrf@users.noreply.github.com>
2025-05-17 13:52:48 +0700

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import GameBoard from "./components/GameBoard";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center p-4">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
